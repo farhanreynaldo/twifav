3dddcfdabd5c53770132ad9eba5216d2c5974b7f
Farhan Reynaldo <farhanreynaldo@gmail.com>
2021-11-14 00:19:41 +0000

    Latest data: Sun Nov 14 00:19:41 UTC 2021

---
+++ data/2021-11-14.jl
@@ -0,0 +1,1 @@
+{"created_at": "Sat Nov 13 16:03:21 +0000 2021", "timestamp": "2021-11-13T16:03:21+00:00", "id": 1459552474906304519, "id_str": "1459552474906304519", "full_text": "@chrisalbon @curvenote is an up and comer", "urls": [], "images": [], "user_id": 88273299, "username": "choldgraf", "link": "https://twitter.com/choldgraf/status/1459552474906304519"}
