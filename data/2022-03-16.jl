4e0de0c58109453d36b34e2325d7f4a2306a4d0d
Farhan Reynaldo <farhanreynaldo@gmail.com>
2022-03-16 00:27:00 +0000

    Latest data: Wed Mar 16 00:27:00 UTC 2022

---
+++ data/2022-03-16.jl
@@ -0,0 +1,1 @@
+{"created_at": "Tue Mar 15 06:45:52 +0000 2022", "timestamp": "2022-03-15T06:45:52+00:00", "id": 1503623496366141440, "id_str": "1503623496366141440", "full_text": "@xynx Wajibb", "urls": [], "images": [], "user_id": 68310549, "username": "RayChris91", "link": "https://twitter.com/RayChris91/status/1503623496366141440"}
