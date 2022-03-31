2f3989983f8eeef34a54f24b300ab800f34dbc09
Farhan Reynaldo <farhanreynaldo@gmail.com>
2022-03-31 00:29:13 +0000

    Latest data: Thu Mar 31 00:29:13 UTC 2022

---
+++ data/2022-03-31.jl
@@ -0,0 +1,1 @@
+{"created_at": "Wed Mar 30 04:18:46 +0000 2022", "timestamp": "2022-03-30T04:18:46+00:00", "id": 1509022295913906178, "id_str": "1509022295913906178", "full_text": "@xynx thank you farhan! Also congrats for the QMSS!", "urls": [], "images": [], "user_id": 1089171449858818048, "username": "alvinulido", "link": "https://twitter.com/alvinulido/status/1509022295913906178"}
